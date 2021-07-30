ba25c5966d46a6b7f2ae05855eb13b68dc41869b
Nathanacus <4307854+Nathanacus@users.noreply.github.com>
2021-07-30 11:11:14 -0500

    Add to-do list, edit navbar, add button to top

---
+++ script.js
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Return to Top Button
+
+// Define button
+const buttonToTop = document.getElementById('buttonToTop');
+
+// Listens for window scrolling
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        buttonToTop.style.display = 'block';
+    } else {
+        buttonToTop.style.display = 'none';
+    }
+}
+
+// Triggered when button is clicked
+function goToTop() {
+    document.body.scrollTop = 0;            // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+}
