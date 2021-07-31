c4d754cfa839d47804cb5ccb078ec47cdfc1bd2a
Nathanacus <4307854+Nathanacus@users.noreply.github.com>
2021-07-30 19:25:31 -0500

    Edit responsive navbar dropdown

--- script.js
+++ script.js
@@ -6,15 +6,32 @@ const buttonToTop = document.getElementById('buttonToTop');
 
 // Listens for window scrolling
 window.onscroll = function() {
+
+    // Activates when user scrolls at least 75px down
     if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
         buttonToTop.style.display = 'block';
     } else {
         buttonToTop.style.display = 'none';
     }
+
 }
 
 // Triggered when button is clicked
 function goToTop() {
     document.body.scrollTop = 0;            // Safari
     document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Responsive Navbar Dropdown
+
+// Toggle navbar display status
+function toggleNavDropdown() {
+    let items = document.getElementById('navbar-dropdown-items');
+    if (items.style.display === 'none') {
+        items.style.display = 'block';
+    } else {
+        items.style.display = 'none';
+    }
+
 }
